d2fe8af34b51d1a1979a089e52f1e60f7e473dec
mr_jay <m33sterjay@gmail.com>
2022-09-04 13:34:52 -0500

    chore: resolved missing lang.object issue - working as intended

---
+++ text-examples/ch2-elementary-programming/ComputeAreaWithConstant.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ComputeAreaWithConstant {
+    public static void main(String[] args) {
+        final double PI = 3.14159; //declare a constant
+        //create a Scanner object
+        Scanner input = new Scanner(System.in);
+        //prompt user for radius
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+        //calculate area
+        double area = radius * radius * PI;
+        //display result
+        System.out.println("The area of circle with radius " + radius + " is " + area);
+    }
+}
