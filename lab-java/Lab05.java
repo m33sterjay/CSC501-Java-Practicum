ea19f39cf407a09620732b9a07960c60ac623c4d
mr_jay <m33sterjay@gmail.com>
2022-09-29 22:36:42 -0500

    chore: slight rearrangement in file structure, names

---
+++ lab-java/Lab05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+// CS501-ol - Lab06 - Jared Robbins
+/** Reverse Integer:
+ * A program that reads an input integer between (0 < x < 2147483647) &&
+ * prints out the integer in reverse order, using only the following:
+ * * System.out.print(), the modulus function (%), the division function (/), a while loop,
+ * * at most one if statement (no else), and a break statement (if needed).
+ * CHALLENGE:
+ * Create a method called 'ReverseInteger' that accepts an integer as an argument
+ * and move working code into the method. Ensure code works identically as before */
+public class Lab05 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // input prompt, assign as int; number
+        System.out.println("Enter a number to reverse:");
+        int number = input.nextInt();
+        // reads input and isolates each digit
+        //
+
+    }
+
+}
